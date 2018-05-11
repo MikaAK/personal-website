30bf8c568941b88005704b57666272787754dbb4
MikaAK <mika@kalathil.me>
2018-05-11 15:35:23 -0700

    fix: navbar links now use anchors and are clickable

--- client/src/app/pages/blog/blog.service.ts
+++ client/src/app/pages/blog/blog.service.ts
@@ -14,21 +14,21 @@ const findPropEq: FindPropEq = curryN(2, compose(_find, propEq))
 export class BlogService {
   /* tslint:disable max-line-length */
   public posts$ = rxOf<BlogPost[]>([{
-    title: 'Hello... World',
+    title: 'Hello... World?',
     slug: 'hello-world',
     imageUrl: 'https://placeimg.com/200/200',
     date: new Date(),
     description: 'The first post of many to come. This post is all about my reasons for starting this blog and inspirations.',
     content: `
       <div>
-        <p class="lh-copy">I’m sure you’ve guessed it if you’re reading this, I’m starting a blog. I like to break things (shout out to @breakingthings), not usually on purpose but it tends to happen when trying out new technologies without doing much research. During the process of fixing things I love to ask questions to the various communities I can get in touch with and figure out how I can do what I’m doing better.</p>
-        <p class="lh-copy mt4">Often time, I’ve been lucky enough to talk with people who work on the source or even authored the library (big thanks to everyone who’ve answered all my dumb questions!), this blog will be an embodiment of the things I’ve learned from these experiences!</p>
+        <p class="lh-copy">I’m sure you’ve guessed it if you’re reading this, I’m starting a blog. I like to break things, not usually on purpose but it tends to happen when trying out new technologies without doing much research. During the process of fixing things I love to ask questions in the related communities and figure out how I can do what I’m doing better.</p>
+        <p class="lh-copy mt4">Often time, I’ve been lucky enough to talk with people who work on the source or even authored the library (big thanks to everyone who’ve answered all my dumb questions 🙌🙌), this blog will be an embodiment of the things I’ve learned from those talks and the experiences I've had hacking</p>
         <p class="lh-copy mt5">Look for things to come on:</p>
         <ul class="mt3 lst-dsc lsp-inside">
           <li class="lh-copy">Angular / React and other JS Frameworks</li>
-          <li class="lh-copy">Elixir</li>
+          <li class="lh-copy">Elixir and GraphQL</li>
           <li class="lh-copy">Devops and AWS</li>
-          <li class="lh-copy">More Randomness</li>
+          <li class="lh-copy">More Randomness 😄</li>
         </ul>
       </div>
     `
