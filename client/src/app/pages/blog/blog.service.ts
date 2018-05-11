009bf1891d194735fc62931bcfea002f25075340
MikaAK <mika@kalathil.me>
2018-05-10 23:39:50 -0700

    feat: load discus comments

--- client/src/app/pages/blog/blog.service.ts
+++ client/src/app/pages/blog/blog.service.ts
@@ -14,12 +14,24 @@ const findPropEq: FindPropEq = curryN(2, compose(_find, propEq))
 export class BlogService {
   /* tslint:disable max-line-length */
   public posts$ = rxOf<BlogPost[]>([{
-    title: 'How to do xyz',
-    slug: 'how-to-do-xzy',
+    title: 'Hello... World',
+    slug: 'hello-world',
     imageUrl: 'https://placeimg.com/200/200',
     date: new Date(),
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel vestibulum arcu. Praesent at vestibulum nibh. Donec in quam quis eros vulputate rhoncus.',
-    content: 'Curabitur ornare auctor nulla in imperdiet. Donec ut metus viverra, sollicitudin magna in, dapibus tellus. Aliquam erat volutpat. Nullam finibus consequat felis quis posuere. <strong>Cras</strong> pharetra ipsum massa, sit amet gravida tortor maximus nec. Duis a lectus consectetur, fermentum nisl ac, consequat massa. Integer ac viverra dui. <h1>Nulla</h1> facilisi. Fusce aliquet iaculis felis at tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+    description: 'The first post of many to come. This post is all about my reasons for starting this blog and inspirations.',
+    content: `
+      <div>
+        <p class="lh-copy">I’m sure you’ve guessed it if you’re reading this, I’m starting a blog. I like to break things (shout out to @breakingthings), not usually on purpose but it tends to happen when trying out new technologies without doing much research. During the process of fixing things I love to ask questions to the various communities I can get in touch with and figure out how I can do what I’m doing better.</p>
+        <p class="lh-copy mt4">Often time, I’ve been lucky enough to talk with people who work on the source or even authored the library (big thanks to everyone who’ve answered all my dumb questions!), this blog will be an embodiment of the things I’ve learned from these experiences!</p>
+        <p class="lh-copy mt5">Look for things to come on:</p>
+        <ul class="mt3 lst-dsc lsp-inside">
+          <li class="lh-copy">Angular / React and other JS Frameworks</li>
+          <li class="lh-copy">Elixir</li>
+          <li class="lh-copy">Devops and AWS</li>
+          <li class="lh-copy">More Randomness</li>
+        </ul>
+      </div>
+    `
   }])
   /* tslint:enable */
 
--- client/src/app/pages/blog/view/view.component.ts
+++ client/src/app/pages/blog/view/view.component.ts
@@ -1,12 +1,30 @@
 import {Component, OnInit} from '@angular/core'
-import {ActivatedRoute} from '@angular/router'
+import {ActivatedRoute, Router} from '@angular/router'
 import {Observable} from 'rxjs/Observable'
-import {mergeMap, map as rxMap} from 'rxjs/operators'
+import {mergeMap, take as rxTake, map as rxMap, tap as rxTap} from 'rxjs/operators'
 import {prop} from 'ramda'
 
 import {BlogPost} from '../../../models'
 
 import {BlogService} from '../blog.service'
+
+const loadComments = (pageUrl: string, postSlug: string) => {
+  (<any>window).disqus_config = function(this: any) {
+    this.page.url = pageUrl
+    this.page.identifier = postSlug
+  };
+
+  /* tslint:disable */
+  (() => {
+    const d = document, s = d.createElement('script');
+    s.src = 'https://mikakalathil.disqus.com/embed.js';
+    (<any>s).setAttribute('data-timestamp', (+new Date()).toString());
+    (d.head || d.body).appendChild(s);
+  })()
+  /* tslint:enable */
+}
+
+const rxMapToSlug = rxMap<BlogPost, string>(prop('slug'))
 
 @Component({
   selector: 'mk-blog-view',
@@ -15,14 +33,36 @@ import {BlogService} from '../blog.service'
 })
 export class BlogViewComponent implements OnInit {
   public post$: Observable<BlogPost>
+  public isCommentsLoaded: boolean
 
-  constructor(private blogService: BlogService, private route: ActivatedRoute) { }
+  constructor(
+    private blogService: BlogService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   public ngOnInit() {
     this.post$ = this.route.params
       .pipe(
-        rxMap<BlogPost, string>(prop('slug')),
-        mergeMap((slug) => this.blogService.getPost(slug))
+        rxMapToSlug,
+        mergeMap((slug) => this.blogService.getPost(slug)),
+        rxTap((post) => {
+          if (!post)
+            this.router.navigateByUrl('/blog')
+        })
       )
+  }
+
+  public loadComments() {
+    this.post$
+      .pipe(
+        rxTake(1),
+        rxMapToSlug
+      )
+      .subscribe((slug) => {
+        this.isCommentsLoaded = true
+
+        loadComments(window.location.href, slug)
+      })
   }
 }
