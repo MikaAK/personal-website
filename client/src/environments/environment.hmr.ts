b728d945a4904ec527daa195a17efc6375176db9
MikaAK <mika@kalathil.me>
2018-06-23 22:32:22 -0700

    wip: update to latest angular

--- client/src/environments/environment.hmr.ts
+++ client/src/environments/environment.hmr.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  hmr: false,
+  hmr: true,
   production: false,
   navbarHeight: 64
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,11 +3,22 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 
 import {AppModule} from './app/app.module'
 import {environment} from './environments/environment'
+import {hmrBootstrap} from './hmr'
 
 if (environment.production)
   enableProdMode()
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch((err) => console.log(err)) // tslint:disable-line no-console
-})
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
+
+if (environment.hmr) {
+  if (module.hot) {
+    hmrBootstrap(module, bootstrap)
+  } else {
+    /* tslint:disable no-console */
+    console.error('HMR is not enabled for webpack-dev-server!')
+    console.log('Are you using the --hmr flag for ng serve?')
+    /* tslint:enable */
+  }
+} else {
+  bootstrap()
+}
